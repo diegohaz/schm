b820c1e5e3447612ceaa0bf19186de1c49f8fc54
Diego Haz <hazdiego@gmail.com>
2018-01-21 22:20:58 -0200

    [koa] Add schm-koa

---
+++ packages/schm-koa/src/index.js
@@ -0,0 +1,95 @@
+// @flow
+import schema from 'schm'
+
+const convertEmptyToTrue = (object: Object): Object => (
+  Object.keys(object).reduce((finalObject, key) => ({
+    ...finalObject,
+    [key]: object[key] === '' ? true : object[key],
+  }), {})
+)
+
+/**
+ * Returns a koa middleware that validates and parses querystring based
+ * on a given schema.
+ * @example
+ * const Koa = require('koa')
+ * const { query } = require('schm-koa')
+ *
+ * const app = new Koa()
+ *
+ * app.use(query({ foo: Boolean, bar: [String] }))
+ *
+ * // request /?foo&bar=1&bar=baz
+ * app.use((ctx) => {
+ *   console.log(ctx.state.query) // { foo: true, bar: ['1', 'baz'] }
+ * })
+ */
+export const query = (params: Object) => async (ctx: Object, next: Function) => {
+  const querySchema = schema(params)
+  const values = convertEmptyToTrue(ctx.query)
+  try {
+    ctx.state.query = await querySchema.validate(values)
+    next()
+  } catch (e) {
+    ctx.state.schmError = true
+    throw e
+  }
+}
+
+/**
+ * Returns a koa middleware that validates and parses request body based
+ * on a given schema.
+ * @example
+ * const Koa = require('koa')
+ * const bodyParser = require('koa-bodyparser')
+ * const { body } = require('schm-koa')
+ *
+ * const app = new Koa()
+ *
+ * app.use(bodyParser())
+ * app.use(body({ foo: Boolean, bar: [String] }))
+ *
+ * // send { foo: 1, bar: 'baz' }
+ * app.use((ctx) => {
+ *   console.log(ctx.state.body) // { foo: true, bar: ['baz'] }
+ * })
+ */
+export const body = (params: Object) => async (ctx: Object, next: Function) => {
+  const bodySchema = schema(params)
+  try {
+    ctx.state.body = await bodySchema.validate(ctx.request.body)
+    next()
+  } catch (e) {
+    ctx.state.schmError = true
+    throw e
+  }
+}
+
+/**
+ * Handles errors from schm-koa.
+ * @example
+ * const Koa = require('koa')
+ * const { query, errorHandler } = require('schm-koa')
+ *
+ * const app = new Koa()
+ *
+ * app.use(errorHandler())
+ * app.use(query({ foo: { type: Boolean, required: true } }))
+ *
+ * // request / without querystring
+ * app.use((ctx) => {
+ *   ...
+ * })
+ *
+ * // it will respond with 400 and error descriptor in response body
+ */
+export const errorHandler = () => async (ctx: Object, next: Function) => {
+  try {
+    await next()
+  } catch (e) {
+    if (ctx.state.schmError) {
+      ctx.body = e
+      ctx.status = 400
+    }
+  }
+}
---
+++ packages/schm-koa/test/index.test.js
@@ -0,0 +1,70 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import request from 'supertest'
+import { query, body, errorHandler } from '../src'
+
+const servers = []
+
+afterAll(() => servers.map(server => server.close()))
+
+const createApp = (middleware) => {
+  const app = new Koa()
+  app.use(bodyParser())
+  app.use(errorHandler())
+  app.use(middleware)
+  app.use((ctx) => {
+    ctx.body = ctx.state
+  })
+  const server = app.listen()
+  servers.push(server)
+  return server
+}
+
+describe('query', () => {
+  it('handles ?foo as foo=true', async () => {
+    const schema = query({ foo: [Boolean] })
+    const app = createApp(schema)
+    const response = await request(app).post('?foo')
+    expect(response.body.query.foo).toEqual([true])
+  })
+
+  it('handles array in querystring', async () => {
+    const schema = query({ foo: [String] })
+    const app = createApp(schema)
+    const response = await request(app).post('?foo=1&foo=baz')
+    expect(response.body.query.foo).toEqual(['1', 'baz'])
+  })
+
+  it('handles errors', async () => {
+    const schema = query({ foo: { type: Boolean, required: true } })
+    const app = createApp(schema)
+    const response = await request(app).post('/')
+    expect(response.status).toBe(400)
+    expect(response.body).toMatchSnapshot()
+  })
+})
+
+describe('body', () => {
+  it('handles schema', async () => {
+    const schema = body({ foo: Boolean })
+    const app = createApp(schema)
+    const response = await request(app).post('/').send({ foo: 1 })
+    expect(response.body.body.foo).toBe(true)
+  })
+
+  it('handles errors', async () => {
+    const schema = body({ foo: { type: Boolean, required: true } })
+    const app = createApp(schema)
+    const response = await request(app).post('/')
+    expect(response.status).toBe(400)
+    expect(response.body).toMatchSnapshot()
+  })
+})
+
+describe('errorHandler', () => {
+  it('passes through', async () => {
+    const app = createApp(() => Promise.reject())
+    const response = await request(app).get('/')
+    expect(response.status).toBe(404)
+  })
+})

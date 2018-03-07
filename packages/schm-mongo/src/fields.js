4fd689cc3088504884c0a2760aab3d7ca86a17bc
Diego Haz <hazdiego@gmail.com>
2018-03-07 07:06:00 -0300

    Add fields schema group

--- packages/schm-mongo/src/fields.js
+++ packages/schm-mongo/src/fields.js
@@ -1,3 +1,22 @@
-const fields = () => {}
+const fields = () => previous =>
+  previous.merge({
+    params: {
+      fields: {
+        type: String,
+        set: value => {
+          if (!value) return value
+          return value
+            .split(',')
+            .map(v => ({
+              [v.replace(/^[+-]/, '')]: v.indexOf('-') === 0 ? 0 : 1,
+            }))
+            .reduce((finalObject, currentObject) => ({
+              ...finalObject,
+              ...currentObject,
+            }))
+        },
+      },
+    },
+  })
 
 export default fields
